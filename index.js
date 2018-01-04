181d2b3f6867a9f8366242dd26d405f05081e9a5
zhangbang <zhangbang@meituan.com>
2018-01-05 01:22:31 +0800

    setup

---
+++ index.js
@@ -0,0 +1,124 @@
+const { Readable } = require('stream');
+const qiniu = require('qiniu');
+const {app, clipboard } = require('electron');
+const fs = require('fs');
+
+const initSuccessMsg = '启动成功，按r键把剪切板里的图片上传到七牛云里吧，按q键退出';
+
+function getServerPosition(code) {
+    switch (code) {
+        // 华东
+        case '0':
+            return qiniu.zone.Zone_z0;
+        // 华北
+        case '1':
+            return qiniu.zone.Zone_z1;
+        // 华南
+        case '2':
+            return qiniu.zone.Zone_z2;
+        // 北美
+        case '3':
+            return qiniu.zone.Zone_na0;
+        default:
+            console.log('找不到你配置的服务器区域代码');
+
+    }
+}
+// should generated to upload image
+var mac = null;
+var uploadToken = null;
+var formUploader = null;
+var putExtra = null;
+function qiniuInit(accessKey, secretKey, bucket, urlDomain, serverPosition) {
+    mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+    var options = {
+      scope: bucket,
+      callbackBody: '{"key":"$(key)","hash":"$(etag)","fsize":$(fsize),"bucket":"$(bucket)","name":"$(x:name)"}',
+      callbackBodyType: 'application/json'
+    };
+    var putPolicy = new qiniu.rs.PutPolicy(options);
+    uploadToken=putPolicy.uploadToken(mac);
+    var config = new qiniu.conf.Config();
+    config.zone = getServerPosition(serverPosition);
+    formUploader = new qiniu.form_up.FormUploader(config);
+    putExtra = new qiniu.form_up.PutExtra();
+}
+
+
+
+function uploadImg(key, readableStream) {
+    formUploader.putStream(uploadToken, key, readableStream, putExtra, function(respErr,
+        respBody, respInfo) {
+            if (respErr) {
+                throw respErr;
+            }
+            if (respInfo.statusCode == 200) {
+                console.log(`上传成功，地址是：http://ov532c17r.bkt.clouddn.com/${key}`);
+                console.log(respBody);
+            } else {
+                console.log(respInfo.statusCode);
+                console.log(respBody);
+            }
+        });
+}
+
+
+
+function doUpload() {
+    const image = clipboard.readImage();
+    // const img = new Tray(image);
+    const imgBuf = image.toPNG();
+    const imageStream = new Readable();
+    imageStream.push(imgBuf);
+    imageStream.push(null);
+    // console.log(imageStream);
+    const key = Math.random().toString(36).substr(2) + '.png';
+    uploadImg(key, imageStream);
+}
+
+
+app.on('ready', function() {
+    process.stdin.setEncoding('utf8');
+    process.stdin.on('end', function() {
+        console.log('工具已退出');
+        app.quit();
+    });
+
+    try {
+        let {
+            accessKey,
+            secretKey,
+            bucket,
+            urlDomain,
+            serverPosition
+        } = require('./config.json');
+        qiniuInit(accessKey, secretKey, bucket, urlDomain, serverPosition);
+        console.log(initSuccessMsg);
+    } catch (e) {
+        console.log('还没有配置，[n]取消，任意键继续');
+        require('./setup');
+    }
+
+
+    process.stdin.on('data', function(chunk) {
+
+        if (chunk === 'e\n') {
+            let {
+                accessKey,
+                secretKey,
+                bucket,
+                urlDomain,
+                serverPosition
+            } = require('./config.json')
+            qiniuInit(accessKey, secretKey, bucket, urlDomain, serverPosition);
+            console.log(initSuccessMsg);
+        }
+        if (chunk === 'r\n') {
+            doUpload();
+        } else if (chunk === 'q\n') {
+            process.stdin.emit('end')
+        } else if (chunk === 'c\n') {
+            doConfig();
+        }
+    })
+})
---
+++ setup.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+
+let configLocal = {};
+// const configString = null;
+
+process.stdin.setEncoding('utf8');
+
+function doConfig() {
+    let cnt = 0;
+    // console.log('配置一下吧[y/n]');
+    process.stdin.on('data', (chunk) => {
+        chunk = chunk.slice(0, -1);
+        if (cnt === 0) {
+            if (chunk === 'n') {
+                process.stdin.emit('end');
+            } else {
+                cnt ++;
+                console.log('accessKey:');
+            }
+        } else if (cnt === 1) {
+            configLocal.accessKey = chunk;
+            cnt ++;
+            console.log('secretKey:');
+        } else if (cnt === 2) {
+            configLocal.secretKey = chunk;
+            cnt ++;
+            console.log('bucket:');
+        } else if (cnt === 3) {
+            configLocal.bucket = chunk;
+            cnt ++;
+            console.log('domain:');
+        } else if (cnt === 4) {
+            configLocal.urlDomain = chunk;
+            cnt ++;
+            console.log('server position: [0:华北 1:华东 2:华南 3:北美]');
+        } else if (cnt === 5) {
+            configLocal.serverPosition = chunk;
+            cnt ++;
+            fs.writeFileSync('./config.json', JSON.stringify(configLocal));
+            console.log('已完成配置，开始愉快地贴图吧, 按e键开始使用');
+            // process.stdin.emit('end');
+        }
+    })
+}
+
+doConfig();
